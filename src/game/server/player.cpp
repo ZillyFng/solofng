c85ba8969282348d68706b2002866f8366fdbd97
ChillerDragon <ChillerDragon@gmail.com>
2020-01-22 18:32:55 +0100

    Do not close file on error (closed #19)

--- src/game/server/player.cpp
+++ src/game/server/player.cpp
@@ -690,7 +690,6 @@ bool CPlayer::SaveStats(const char *pFilePath, bool Failed)
 		if (trys > max_trys)
 		{
 			dbg_msg("stats", "error: locked file (trys %d/%d) failed=%d path='%s'", trys, max_trys, Failed, aLockPath);
-			fclose(pFile);
 			pFile = NULL;
 			GameServer()->m_StatSaveFails++;
 			if (Failed)
